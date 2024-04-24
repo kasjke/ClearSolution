9ed8cbafbb0497d640914dd8b0f87348f9e0ac51
kasjke <kasmatuy2k17@gmail.com>
2024-04-25 00:10:39 +0300

    first

---
+++ src/main/java/com/example/clearsolutions/User.java
@@ -0,0 +1,4 @@
+package com.example.clearsolutions;
+
+public class User {
+}
---
+++ src/test/java/com/example/clearsolutions/ClearSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clearsolutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
